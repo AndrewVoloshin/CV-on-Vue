c4436d8fb69c6f4e2d0db80bec778009cc3a18fe
AndrewVoloshin <voloshinandy@gmai.com>
2022-10-21 12:27:08 +0300

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header">
+    <h1>here</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <app-header> </app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  name: "Home",
+  components: {
+    AppHeader,
+  },
+};
+</script>
