7878246454d94e30838ebe55b5040de9d352dfb4
AndrewVoloshin <voloshinandy@gmai.com>
2023-11-02 18:59:29 +0200

    feat: rebuild packege.json

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -13,6 +12,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,7 @@ import AppPortfolio from "@/components/AppPortfolio.vue";
 import AppFooter from "@/components/AppFooter.vue";
 
 export default {
-  name: "Home",
+  name: "HomeView",
   components: {
     AppHeader,
     AppIntro,
